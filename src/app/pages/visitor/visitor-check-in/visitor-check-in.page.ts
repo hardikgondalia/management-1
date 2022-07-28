cc9b65a58ef9ce7f86a857fca50745eba580651b
bhagyeshboraleacm <misty6050@gmail.com>
2022-07-28 14:26:00 +0530

    Initialization

---
+++ src/app/pages/visitor/visitor-check-in/visitor-check-in.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visitor-check-in',
+  templateUrl: './visitor-check-in.page.html',
+  styleUrls: ['./visitor-check-in.page.scss'],
+})
+export class VisitorCheckInPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/visitor/visitor-check-out/visitor-check-out.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visitor-check-out',
+  templateUrl: './visitor-check-out.page.html',
+  styleUrls: ['./visitor-check-out.page.scss'],
+})
+export class VisitorCheckOutPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
